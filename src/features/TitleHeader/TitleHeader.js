c506a37b0839979cd64afafddd83c31bc459436d
Tom-Sloan <tsloan81@gmail.com>
2021-06-05 19:51:05 -0400

    added changing icon for column layyout

--- src/features/TitleHeader/TitleHeader.js
+++ src/features/TitleHeader/TitleHeader.js
@@ -1,10 +1,21 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import styles from './TitleHeader.module.css'
-import {SubredditsBar} from '../Subreddit/SubredditsBar'
+import {selectColumnNumber, selectImageLink, updateTitleHeader} from './titleHeaderSlice'
+
 
 export function TitleHeader() {
   
+  const dispatch = useDispatch();
+  const columnNumber = useSelector(selectColumnNumber)
+  const columnLayoutImage = useSelector(selectImageLink)
+  
+  const handleClick = (e)=>{
+    console.log("clicked!")
+    console.log(columnNumber)
+    console.log(columnLayoutImage)
+    dispatch(updateTitleHeader())
+  }
   return (
     <div className={styles.titleHeader}>
       <div className={styles.smallHeaderOption}>
@@ -14,7 +25,7 @@ export function TitleHeader() {
         <p style={{backgroundColor:'orange', width:'100%', marginBlockStart: '0em', marginBlockEnd: '0em'}}>hey</p>
       </div>
       <div className={styles.smallHeaderOption}>
-        <img className = {styles.logoImage} src="/./grid-icon_2.png" alt="redditProfit" />
+        <img onClick={handleClick} className = {`${styles.logoImage} ${styles.changeLayoutIcon}`} src={columnLayoutImage} alt="redditProfit" />
       </div>
     </div>
   );
--- src/features/TitleHeader/titleHeaderSlice.js
+++ src/features/TitleHeader/titleHeaderSlice.js
@@ -3,25 +3,32 @@ import { createAsyncThunk, createSlice, current } from '@reduxjs/toolkit';
 export const titleHeaderSlice = createSlice({
     name: 'titleHeader',
     initialState: {
-        columnNumber: 1 ,
-        columnImg: [
-
-        ]
-
+        data:{
+            columnNumber: 1 ,
+            columnImg: [
+                "/./resources/grid-icon_1.png",
+                "/./resources/grid-icon_2.png",
+                "/./resources/grid-icon_3.png",
+                "/./resources/grid-icon_4.png",   
+            ]
+        }
     },
     // The `reducers` field lets us define reducers and generate associated actions
     reducers: {
         updateTitleHeader: (state) => {
-            state = {
-                ...state,
-                columnNumber: state.columnNumber % 5 + 1
+            console.log(current(state))
+            state.data = {
+                ...state.data,
+                columnNumber: state.data.columnNumber % 4 + 1,
             }
+            console.log(current(state))
         },
     },
   });
   
 // export const selectTitleHeaderJsonData = (state) => state.titleHeader.subjson;
 export const {updateTitleHeader} = titleHeaderSlice.actions;
-export const selectImageLink = (state) => state.titleHeader.columnImg[state.titleHeader.columnNumber-1];
+export const selectImageLink = (state) => state.titleHeader.data.columnImg[state.titleHeader.data.columnNumber-1];
+export const selectColumnNumber= (state) => state.titleHeader.data.columnNumber ;
 
 export default titleHeaderSlice.reducer;
