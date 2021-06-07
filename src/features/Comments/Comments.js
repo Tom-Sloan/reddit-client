c10f82f67318e21a3145f83889e561e8b132433f
Tom-Sloan <tsloan81@gmail.com>
2021-06-07 08:39:53 -0400

    added limit on 5 comments then scroll

--- src/features/Comments/Comments.js
+++ src/features/Comments/Comments.js
@@ -1,25 +1,40 @@
 import React, { useState, useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import styles from '../../components/bodyStyles.module.css';
+import styles from './Comments.module.css';
 import { selectComments } from './commentsSlice';
-import {selectCurrentSubredditName} from '../Subreddit/subredditSlice'
 
 export function Comments({postId}) {
+    const [innerHeight, setHeight] = useState(0);
     const allComments = useSelector(selectComments);
 
     let element;
     console.log('allcomments')
     console.log(allComments)
     if( allComments[postId].length > 0) {
-        element = allComments[postId].map(comment => (<div className={styles.comment}><p>{comment}</p><hr/></div>));
+        element = allComments[postId].map((comment, index) => (
+        <div id={'comment_'+postId+'-'+index} className={styles.comment}>
+            <p>{comment}</p>
+            <hr/>
+        </div>));
+        
     } else {
         element = <div>No Comments</div>;
     }
 
+    useEffect(()=>{
+        let height = 0;
+        let limit = allComments[postId].length > 4? 5 :  allComments[postId].length
+        for( let i = 0; i < limit; i++){
+            console.log('height')
+            console.log(document.getElementById('comment_'+postId+'-'+i).clientHeight)
+            height += document.getElementById('comment_'+postId+'-'+i).clientHeight;
+        }
+        setHeight(height)
+    }, [])
     // console.log(element)
 
     return (
-        <div key= {'comment_'+postId} className={styles.comments}>
+        <div style={{height: innerHeight + 40}} key= {'comment_'+postId} className={styles.comments}>
             {element}
         </div>
     )
